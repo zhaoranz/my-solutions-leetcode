e3b7f2da76b2c666cba0569460b5dafce79a7932
zhaoranz <zhaoran1124@gmail.com>
2021-01-16 15:45:47 +0100

    Update 283 Move Zeroes.py

--- 283 Move Zeroes.py
+++ 283 Move Zeroes.py
@@ -10,3 +10,12 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
                 i+= 1
             j += 1
+            
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(nums.count(0)):
+            nums.remove(0)
+            nums.append(0)
