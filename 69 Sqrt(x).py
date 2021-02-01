fd0824d077b795dd5b53ad404e13176a0029f0da
zhaoranz <zhaoran1124@gmail.com>
2021-02-01 13:46:31 +0100

    Create 69 Sqrt(x).py

---
+++ 69 Sqrt(x).py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r, ans = 0, x, -1
+        
+        while l <= r: 
+            mid = l +(r-l)//2
+            if mid * mid <= x:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
+        
