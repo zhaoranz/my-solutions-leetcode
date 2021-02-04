fe6a721f91dd925c9859e708203ac67c921b85f4
zhaoranz <zhaoran1124@gmail.com>
2021-02-04 15:48:17 +0100

    Update Longest Harmonious Subsequence.py

--- Longest Harmonious Subsequence.py
+++ Longest Harmonious Subsequence.py
@@ -9,4 +9,16 @@ class Solution:
                 ans = max(ans, r - l + 1)
                 
         return ans
-        
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        dic1 = {}
+        ans = 0
+        for num in nums:
+            dic1[num] = dic1.get(num,0) + 1
+        for key in dic1:
+            if (key + 1) in dic1:
+                ans = max(ans, dic1[key] + dic1[key+1])
+
+                
+        return ans
+                        
