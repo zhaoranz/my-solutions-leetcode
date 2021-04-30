2d4bb62493846842b88f0810504be6f24aab2a06
zhaoranz <zhaoran1124@gmail.com>
2021-04-30 13:40:52 +0200

    Update Powerful Integers.py

--- Powerful Integers.py
+++ Powerful Integers.py
@@ -34,3 +34,15 @@ class Solution:
             
         return list(res)
         
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        res , i , j = set() , 0 , 0
+        while x**i<bound:
+            while x**i + y**j <= bound:
+                res.add(x**i+y**j)
+                j+=1
+                if y==1:  break
+            i+=1
+            j = 0
+            if x==1: break
+        return list(res)
