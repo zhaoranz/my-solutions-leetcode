1c2d19b34aa35d0830d7e8c365dea0f9e17c2289
zhaoranz <zhaoran1124@gmail.com>
2021-05-14 17:30:09 +0200

    Update Ambiguous Coordinates.py

--- Ambiguous Coordinates.py
+++ Ambiguous Coordinates.py
@@ -21,3 +21,19 @@ class Solution:
                 for j in y:
                     ans.append('('+i+', '+j+')')
         return ans
+#solution-2
+class Solution:
+    def ambiguousCoordinates(self, s: str) -> List[str]:
+        def make(frag):
+            N = len(frag)
+            for d in range(1, N+1):
+                left = frag[:d]
+                right = frag[d:]
+                if ((not left.startswith('0') or left == '0')
+                        and (not right.endswith('0'))):
+                    yield left + ('.' if d != N else '') + right
+
+        s = s[1:-1]
+        return ["({}, {})".format(*cand)
+                for i in range(1, len(s))
+                for cand in itertools.product(make(s[:i]), make(s[i:]))]
