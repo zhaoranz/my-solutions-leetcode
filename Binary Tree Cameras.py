31feac5686111a40b9b8a8d1a59aa78a944a5e04
zhaoranz <zhaoran1124@gmail.com>
2021-05-16 12:49:50 +0200

    Create Binary Tree Cameras.py

---
+++ Binary Tree Cameras.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: TreeNode) -> int:
+        def dfs(root: TreeNode) -> List[int]:
+            if not root:
+                return [float('inf'),0,0]
+            la,lb,lc = dfs(root.left)
+            ra,rb,rc = dfs(root.right)
+            a = lc + rc + 1
+            b = min(a, la+rb, ra+lb)
+            c = min(a, lb+rb)
+            
+            return [a,b,c]
+        a, b, c = dfs(root)
+        return b
