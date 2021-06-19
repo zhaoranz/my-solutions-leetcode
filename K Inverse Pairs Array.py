9971856bc082fb38e59a4c902ad85828fcdc7d3b
zhaoranz <zhaoran1124@gmail.com>
2021-06-19 17:26:58 +0200

    Create K Inverse Pairs Array.py

---
+++ K Inverse Pairs Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        MOD = 10**9 + 7
+        dp =[[0]*(k+1) for _ in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0]=1
+        for i in range(1, n+1):
+            for j in range(1, k+1):
+                dp[i][j] += dp[i][j-1]+dp[i-1][j]
+                if j-i >=0:
+                    dp[i][j] -= dp[i-1][j-i]
+                dp[i][j] %= MOD
+        return dp[n][k]
