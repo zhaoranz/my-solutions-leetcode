a6916d8cd30fc439878f2e4998ce6aca21d6e338
zhaoranz <zhaoran1124@gmail.com>
2021-06-22 12:18:26 +0200

    Update Number of Matching Subsequences.py

--- Number of Matching Subsequences.py
+++ Number of Matching Subsequences.py
@@ -24,3 +24,15 @@ class Solution:
 
                     
         return res
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        count = 0
+        for i, c in Counter(words).items():
+            indexj = 0
+            for j in i:
+                indexj = s.find(j, indexj) + 1
+                if not indexj:
+                    break
+            else:
+                count +=c
+        return count
