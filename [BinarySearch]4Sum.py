24649b937ddfaa47afa242c13bfda0a251de94a5
zhaoranz <zhaoran1124@gmail.com>
2021-07-16 16:55:55 +0200

    Update [BinarySearch]4Sum.py

--- [BinarySearch]4Sum.py
+++ [BinarySearch]4Sum.py
@@ -25,4 +25,33 @@ class Solution:
                 j = numNext[j]
             i = numNext[i]
         return res
+###Dobule pointers:
 
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        if n < 4:
+            return []
+        res =[]
+        nums.sort()
+        for i in range(n-3):
+            if i == 0 or nums[i]!=nums[i-1]:
+                for j in range(i+1, n-2):
+                    if j ==i+1 or nums[j]!=nums[j-1]:
+                        l,r = j+1, n-1
+                        nt= target -nums[i]-nums[j]
+                        while l < r:
+                            if nums[l]+nums[r]==nt:
+                                res.append([nums[i],nums[j],nums[l],nums[r]])
+                                while l < r and nums[l]==nums[l+1]:
+                                    l+=1
+                                l+=1
+                                while l < r and nums[r]==nums[r-1]:
+                                    r-=1
+                                r-=1
+                            elif nums[l]+nums[r]>nt:
+                                r-=1
+                            else:
+                                l+=1
+        return res
+                    
