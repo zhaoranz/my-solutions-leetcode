ffcec358b16ff8924d424549b06e47cdf0538f47
zhaoranz <zhaoran1124@gmail.com>
2021-08-12 17:45:08 +0200

    Create [Hash]Group Anagrams.py

---
+++ [Hash]Group Anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp=defaultdict(list)
+        for st in strs:
+            counts=[0]*26
+            for ch in st:
+                counts[ord(ch)-ord('a')]+=1
+            mp[tuple(counts)].append(st)
+        return list(mp.values())
