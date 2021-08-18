aa86ec59e896db36ec6c3d6b38ea249defb03433
zhaoranz <zhaoran1124@gmail.com>
2021-08-18 15:20:34 +0200

    Update [DP]Decode Ways.py

--- [DP]Decode Ways.py
+++ [DP]Decode Ways.py
@@ -11,3 +11,18 @@ class Solution:
                 c += a
             a, b = b, c
         return c
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0]=='0':
+            return 0
+        n = len(s)
+        dp = [0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
+            if s[i-1]!='0':
+                dp[i]+=dp[i-1]
+            if 10<=int(s[i-2:i])<=26:
+                dp[i]+=dp[i-2]
+        return dp[-1]
+    
