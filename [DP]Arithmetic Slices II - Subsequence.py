6a6b2936dcad8993733b4b2c9568c22dc2a798e1
zhaoranz <zhaoran1124@gmail.com>
2021-09-10 15:09:47 +0200

    Update [DP]Arithmetic Slices II - Subsequence.py

--- [DP]Arithmetic Slices II - Subsequence.py
+++ [DP]Arithmetic Slices II - Subsequence.py
@@ -7,3 +7,22 @@
                 ans += cnt
                 f[i][d] += cnt + 1
         return ans
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        size = len(nums)
+        if size < 3:
+            return 0
+        
+        dp = [defaultdict(int) for _ in range(size)]
+        res = 0
+        for i in range(size):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] += dp[j][diff] + 1
+                if diff in dp[j]:
+                    res += dp[j][diff]
+        return res
