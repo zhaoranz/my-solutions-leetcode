6604ecaf559a283332ac5cd835e85cb07105168f
zhaoranz <zhaoran1124@gmail.com>
2021-09-23 15:50:27 +0200

    Update Max Area of Island.py

--- Max Area of Island.py
+++ Max Area of Island.py
@@ -1,3 +1,29 @@
+#BFS
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        res=0
+        dirs=[(0,1),(0,-1),(1,0),(-1,0)]
+        m,n=len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]!=1:
+                    continue
+                cnt=0
+                q=[(i,j)]
+                grid[i][j]*=-1
+                while q:
+                    x,y=q.pop(0)
+                    cnt+=1
+                    res=max(res, cnt)
+                    for direct in dirs:
+                        X,Y=x+direct[0],y+direct[1]
+                        if 0<=X<m and 0<=Y<n and grid[X][Y]>0:
+                            grid[X][Y] *=-1
+                            q.append((X,Y))
+        return res
+                
+                        
+#DFS
 class Solution:
     def dfs(self, grid, cur_i, cur_j) -> int:
         if cur_i < 0 or cur_j < 0 or cur_i == len(grid) or cur_j == len(grid[0]) or grid[cur_i][cur_j] !=1:
