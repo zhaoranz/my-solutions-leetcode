d3b5b9c160a271f804dc11064b16290fb8125058
zhaoranz <zhaoran1124@gmail.com>
2021-09-24 14:53:02 +0200

    Create [recursionLinkedList]21 Merge Two Sorted Lists.py

---
+++ [recursionLinkedList]21 Merge Two Sorted Lists.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode(-1)
+        cur=dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next=l1
+                l1=l1.next
+            else:
+                cur.next=l2
+                l2=l2.next
+            cur=cur.next
+        cur.next=l1 if l1 else l2
+        return dummy.next
+        
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val<=l2.val:
+            l1.next=self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next=self.mergeTwoLists(l1,l2.next)
+            return l2
