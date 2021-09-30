560e546a6e982ee0f44e8a83d44410d7a36352a0
zhaoranz <zhaoran1124@gmail.com>
2021-09-30 17:08:00 +0200

    Update [DP_Backtrack]698Partition to K Equal Sum Subsets.py

--- [DP_Backtrack]698Partition to K Equal Sum Subsets.py
+++ [DP_Backtrack]698Partition to K Equal Sum Subsets.py
@@ -33,7 +33,39 @@ class Solution:
                     i -=1 #dinstinct, if ith element cannot satisfy, then other elements with the same value cannot satisfy either
             return False
         return dfs(nums, n-1, target, 0, k, used)
-      
+ #backTracking:
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        
+        s = sum(nums)
+        if s % k != 0:
+            return False
+        target = s // k
+        print(target)
+        visited = [False] * len(nums)
+        nums.sort(reverse=True)
+        
+        def backtrack(start, currSum, currK):
+            if currK == 1:
+                return True
+            
+            if currSum == target:
+                #print(start, currK)
+                return backtrack(0, 0, currK-1)
+            
+            for i in range(start, len(nums)):
+                if not visited[i] and currSum + nums[i] <= target:
+                    visited[i] = True
+                    #print(i, currSum, currK, visited)
+                    if backtrack(i + 1, currSum + nums[i], currK):
+                        return True
+                    visited[i] = False
+                    
+            return False
+        
+        return backtrack(0, 0, k)
+    
+    
       #DP:slower than the backtracking.
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
