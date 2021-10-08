9f6be95e6e0031595186d97cea54010bd209395b
zhaoranz <zhaoran1124@gmail.com>
2021-10-08 13:45:37 +0200

    Update 208. Implement Trie (Prefix Tree).py

--- 208. Implement Trie (Prefix Tree).py
+++ 208. Implement Trie (Prefix Tree).py
@@ -41,3 +41,41 @@ class Trie:
             node=node.children[idx]
         return node is not None
         
+class Trie:
+
+    def __init__(self):
+        """ Initialize your data structure here. """
+        self.dic = {}
+
+    def insert(self, word: str) -> None:
+        """ Inserts a word into the trie. """
+        cur = self.dic
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur['#'] = True
+
+    def search(self, word: str) -> bool:
+        """ Returns if the word is in the trie. """
+        cur = self.dic
+        for c in word:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return '#' in cur
+
+    def startsWith(self, prefix: str) -> bool:
+        """ Returns if there is any word in the trie that starts with the given prefix. """
+        cur = self.dic
+        for c in prefix:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
